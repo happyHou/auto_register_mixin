7eb5ddc5a8ea86dbeeba218032b66355ad1c874c
xixiHou <hoululin@gmail.com>
2017-12-06 14:48:10 +0800

    add phone proxy web site

--- auto_xin.py
+++ auto_xin.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import bs4
 import requests, pickle
 import requests.packages.urllib3
@@ -25,6 +27,11 @@ class AUTO_XIN(object):
         self.sendSMSUrl = 'https://api.mixin.one/verifications/'
         self.setUserNameUrl = 'https://api.mixin.one/me'
 
+        self.proxies = {
+            "http": "127.0.0.1:8888",
+            "https": "127.0.0.1:8888",
+        }
+
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36',
             'ContentType': 'text/html; charset=utf-8',
@@ -32,19 +39,10 @@ class AUTO_XIN(object):
             'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
             'Connection': 'keep-alive',
         }
-        self.proxyDict = {
-            "http": 'http://127.0.0.1:8888',
-            "https": 'https://127.0.0.1:8888',
-        }
         self.generation = {
             "alg": "RS512",
             "typ": "JWT"
         }
-
-        self.sess = requests.Session()
-        self.sess.proxies.update(self.proxyDict)
-        self.sess.headers.update(self.headers)
-        self.sess.verify = "./charles-ssl-proxying-certificate.pem"
 
         self.cookies = {
 
@@ -53,13 +51,18 @@ class AUTO_XIN(object):
 
         }
 
+        self.sess = requests.Session()
+        self.sess.verify = './charles-ssl-proxying-certificate.pem'
+        self.sess.proxies = self.proxies
+
     # get __cfduid
     def getCookie(self, options):
         print '+++++++++++setp1+++++++++++'
         print '+++++++++++_cfduid begin+++++++++++'
         url = self.registerUrl + options.invitationCode
         resp = self.sess.get(
-            url
+            url,
+            headers=self.headers,
         )
         print resp.cookies
 
@@ -69,16 +72,13 @@ class AUTO_XIN(object):
         print '+++++++++++step2+++++++++++'
         print '+++++++++++send SMS begin+++++++++++'
         payload = {'phone': options.phone, 'purpose': 'SESSION'}
-        header = self.headers
-        header['ContentType'] = 'application/json'
-        header['Accept'] = '*/*'
-        header['Origin'] = 'https://mixin.one'
-        header['Authorization'] = 'Bearer'
-        header['Referer'] = 'https://mixin.one/enroll/' + options.invitationCode
+        # self.headers['Referer'] = self.registerUrl + options.invitationCode
+        # self.headers['Origin'] = 'https://mixin.one'
+        # self.headers['Authorization'] = 'Bearer'
         resp = self.sess.post(
             self.sendSMSUrl,
-            data=json.dumps(payload),
-            headers=header
+            headers=self.headers,
+            json=payload
         )
         print resp.text
         rs = json.loads(resp.text)
@@ -148,13 +148,86 @@ class AUTO_XIN(object):
         print resp.text
 
 
+class EMA666(object):
+    def __init__(self):
+        self.user = {
+            'userName': 'chg999',
+            'passWord': '123456',
+            'ItemId': '58012',
+        }
+        self.loginUrl = 'http://api.ema666.com/Api/userLogin'
+        self.getItemUrl = 'http://api.ema666.com/Api/userGetItems'
+        self.getPhoneUrl = 'http://api.ema666.com/Api/userGetPhone'
+        self.getMessageUrl = 'http://api.ema666.com/Api/userSingleGetMessage'
+        self.sess = requests.Session()
+        self.token = {}
+
+    def login(self):
+        payload = {'uName': self.user['userName'],
+                   'pWord': self.user['passWord'],
+                   'Code': 'UTF8'
+                   }
+        resp = self.sess.get(
+            self.loginUrl,
+            params=payload)
+        rs = resp.text.split('&')
+        self.token = rs[0]
+
+        print u'token：{0}'.format(rs[0])
+        print u'账户余额：{0}'.format(rs[1])
+        print u'最大登录客户端：{0}'.format(rs[2])
+        print u'最多获取号码数：{0}'.format(rs[3])
+        print u'单个客户端最多获取号码数：{0}'.format(rs[4])
+        print u'折扣：{0}'.format(rs[5])
+
+    # 暂时用不到
+    def getItems(self):
+        payload = {'token': self.token,
+                   'tp': 'ut',
+                   'Code': 'UTF8'
+                   }
+        resp = self.sess.get(
+            self.getItemUrl,
+            params=payload
+        )
+        print resp.text
+
+    def getPhone(self):
+        payload = {'token': self.token,
+                   'ItemId': self.user['ItemId'],
+                   'Code': 'UTF8'
+                   }
+        resp = self.sess.get(
+            self.getPhoneUrl,
+            params=payload
+        )
+        return '+86' + resp.text[0: -1], resp.text[0:-1]
+
+    def getMessage(self, options):
+        payload = {'token': self.token,
+                   'ItemId': self.user['ItemId'],
+                   'phone': options.truePhone
+                   }
+        resp = self.sess.get(
+            self.getMessageUrl,
+            params=payload
+        )
+        print resp.text
+
+
 def main(options):
     xin = AUTO_XIN()
-    # xin.getCookie(options)
-    # xin.sendSMS(options)
+    # xin.demoHttps()
+    # ema666 = EMA666()
+    # ema666.login()
+    # phone_arr = ema666.getPhone()[0]
+    # options.phone = phone_arr[0]
+    # options.truePhone = phone_arr[1]
+    xin.getCookie(options)
+    xin.sendSMS(options)
+    # ema666.getMessage(options);
     # xin.verifications(options)
     # xin.setUserName(options)
-    xin.demoHttps()
 
 
 if __name__ == '__main__':
@@ -162,7 +235,7 @@ if __name__ == '__main__':
         description='Simulate to login Jing Dong, and buy sepecified good')
     options = parser.parse_args()
     options.invitationCode = '802847'
-    options.phone = '+8613466334619'
+    options.phone = '+8615020978071'
     options.code = '0904'
     options.name = 'random99'
 
